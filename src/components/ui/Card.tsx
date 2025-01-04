ece3d336d7c4ed8caa379dd13bbb8d73bfe7e5de
shaffi-ahuja <shaffi.ahuja@gmail.com>
2025-01-05 00:23:18 +0530

    techstack div update

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -4,15 +4,22 @@ import React from 'react'
 const Card = ({ image, skills, title, description }: { image?: string, skills?: any, title: string, description: any }) => {
     return (
         <div className='card'>
-            {image && <div className='card-image-div'>
-                <Image className="card-image" src={image} alt='Card Image' height={320} width={320} />
-            </div>}
-            {skills && <div className="skill-card-image-div">
-                {skills.map((skill: any) => (
-                    <img key={skill} src={`https://skillicons.dev/icons?i=${skill}`} />
-                ))}
+            {image
+                &&
+                <div className='card-image-div'>
+                    <Image className="card-image" src={image} alt='Card Image' height={320} width={320} />
+                </div>
+            }
+            {skills
+                &&
+                <div className='xl:grow content-center'>
+                    <div className="skill-card-image-div">
+                        {skills.map((skill: any) => (
+                            <img key={skill} src={`https://skillicons.dev/icons?i=${skill}`} />
+                        ))}
 
-            </div>
+                    </div>
+                </div>
             }
             <div className='card-title'>{title}</div>
             <div >{description}</div>
